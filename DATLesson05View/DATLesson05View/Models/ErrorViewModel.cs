db72cb179dfca59254bb4ae3960cd99d8660f1e8
dinhanhtuan <anh974086@gmail.com>
2026-09-06 11:59:36 +0700

    Hoan thanh Bai 05 View - Sinh vien DAT

---
+++ DATLesson05View/DATLesson05View/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DATLesson05View.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
