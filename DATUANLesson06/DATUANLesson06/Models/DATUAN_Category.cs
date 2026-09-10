62119528ff4a9f7beb89eca9450223610dc825c4
dinhanhtuan <anh974086@gmail.com>
2026-09-10 08:44:15 +0700

    DATUAN - Hoan thanh Bai thuc hanh Lesson 06

---
+++ DATUANLesson06/DATUANLesson06/Models/DATUAN_Category.cs
@@ -0,0 +1,9 @@
+﻿namespace DATUANLesson06.Models
+{
+    public class DATUAN_Category
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; } = string.Empty;
+        public bool IsActive { get; set; }
+    }
+}
---
+++ DATUANLesson06/DATUANLesson06/ViewComponents/DATUAN_CategoryViewComponent.cs
@@ -0,0 +1,28 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using DATUANLesson06.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DATUANLesson06.ViewComponents
+{
+    public class DATUAN_CategoryViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(int? n)
+        {
+            int limit = n ?? 0;
+
+            var categories = new List<DATUAN_Category>
+            {
+                new DATUAN_Category { CategoryId = 1, CategoryName = "Đồ gia dụng", IsActive = true },
+                new DATUAN_Category { CategoryId = 2, CategoryName = "Thiết bị điện tử", IsActive = true },
+                new DATUAN_Category { CategoryId = 3, CategoryName = "Quần áo thời trang", IsActive = true },
+                new DATUAN_Category { CategoryId = 4, CategoryName = "Y tế & Sức khỏe", IsActive = true }
+            };
+
+            var result = categories.Where(c => c.CategoryId > limit).ToList();
+
+            return View(result);
+        }
+    }
+}
