19b579e88d31c4a8d3918b552db0e7653f9e1835
dinhanhtuan <anh974086@gmail.com>
2026-09-12 19:32:48 +0700

    DATUAN - Hoan thanh Lesson 07

---
+++ DAT_Lesson07_Model/DAT_Lesson07_Model/Controllers/DAT_MemberController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using DAT_Lesson07_Model.DataModel;
+
+namespace DAT_Lesson07_Model.Controllers
+{
+    public class DAT_MemberController : Controller
+    {
+        // Khai báo danh sách static lưu dữ liệu trong bộ nhớ
+        protected static List<DAT_Member> memberList = new List<DAT_Member>()
+        {
+            new DAT_Member(Guid.NewGuid().ToString(), "datuser", "123", "ĐINH ANH TUẤN", "dat02376@gmail.com"),
+            new DAT_Member(Guid.NewGuid().ToString(), "user1", "123", "Nguyễn Văn A", "nva@gmail.com"),
+            new DAT_Member(Guid.NewGuid().ToString(), "user2", "123", "Trần Thị B", "ttb@gmail.com")
+        };
+
+        // 1. Action hiển thị danh sách (Strong Typing)
+        public IActionResult Index()
+        {
+            return View(memberList);
+        }
+
+        // 2. Action trả về giao diện thêm mới (GET)
+        public IActionResult CreateMember()
+        {
+            return View();
+        }
+
+        // 3. Action xử lý nhận dữ liệu từ Form (POST)
+        [HttpPost]
+        public IActionResult CreateMember(DAT_Member member)
+        {
+            // Tự động sinh GUID cho ID
+            member.DAT_MemberID = Guid.NewGuid().ToString();
+
+            // Thêm đối tượng vào danh sách
+            memberList.Add(member);
+
+            // Quay về trang danh sách
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ DAT_Lesson07_Model/DAT_Lesson07_Model/DataModel/DAT_Member.cs
@@ -0,0 +1,26 @@
+﻿namespace DAT_Lesson07_Model.DataModel
+{
+    public class DAT_Member
+    {
+        public string DAT_MemberID { get; set; }
+        public string DAT_Username { get; set; }
+        public string DAT_Password { get; set; }
+        public string DAT_FullName { get; set; }
+        public string DAT_Email { get; set; }
+
+        // BẮT BUỘC: Constructor không tham số dành cho Model Binder
+        public DAT_Member()
+        {
+        }
+
+        // Constructor có tham số (nếu có)
+        public DAT_Member(string id, string username, string password, string fullname, string email)
+        {
+            DAT_MemberID = id;
+            DAT_Username = username;
+            DAT_Password = password;
+            DAT_FullName = fullname;
+            DAT_Email = email;
+        }
+    }
+}
